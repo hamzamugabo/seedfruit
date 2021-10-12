35b084e64002dce81297b327e15b7c24688b2599
hamza mugabo <hamzamugabo3@gmail.com>
2021-10-12 15:06:58 +0300

    rows

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
     <meta name="description" content="seedfruit is a platform for secure agricultural nonfungibale tokens.">
-    <!-- <link href="css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link href="css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>    
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>     -->
 </head>
     <body>
 
@@ -115,11 +115,11 @@
     </nav>
 </div>
 
-<div class="container"">
+<div class="container" style="padding-bottom:100px">
 <div class="row" style="padding-top:50px ;padding-bottom:50px; border-bottom: 6px solid #767073">
     <div class="col-md-6" >
         <div>
-        <strong> <h3>   SeedFruit Idea: </h3></strong><br>
+        <strong> <h3> <img src="images/idea.png" alt="Idea" style="width:94px;height:94px;">  SeedFruit Idea: </h3></strong><br>
 
 <div style="font-size: larger;font-weight:500px;padding-bottom:30px;">
       
@@ -136,7 +136,7 @@ to stay profitable since they are faring on small plots of land which makes it d
 for them to even break even
         </div>
         </div>
-       <strong> <h3> SeedFruit concept or solution detail </h3></strong><br>
+       <strong> <h3><img src="images/soln.jpg" alt="Idea" style="width:94px;height:94px;"> SeedFruit solution detail </h3></strong><br>
 <div style="font-size: larger;font-weight:500px;">
 the proposed solution is a blockchain theory archtectual design process which looks
 at and engages the enitre agricultrure ecosystem and simulates it on the blokchain
@@ -148,14 +148,46 @@ on a stable token
 <img src="images/blockchain.jpg" alt="Block chain" style="width:500px;height:400px;">
 </div>
 </div>
-<div class="row">
 <div class="row" style="padding-top:50px ;padding-bottom:50px;">
-
+<div style="align-items: center;width:100%;margin:auto;">
+<div>
+<strong style="align-items: center;"> <h3><img src="images/disclaimer.jpg" alt="Idea" style="width:94px;height:94px;"> Disclaimer </h3></strong><br>
+</div>
+This is the first draft of the seedfruit protocol and the obligations of the tokenied coin
+is to keep the stable coin in an evaluation that is relative to the underlayng assets and
+the actual valuation of those asets in line with what it is that is being done on ground
+and what should be expected from the output gains and return of theunderlaying asets.
+Obligations of investors buyinginto the seedfruit crypto tokenised coin is for the
+nvestor to make sure they have actual holding and the rriht daily valuation of the
+tokens
+the investor holding the tokens should also ask for audited books on quaterly or
+annual basis as agreed to with the seedfruit team so that they acertainthe actaula value
+thats underlaying the coins they are in current pssation of
+the investr is also resposible for knowing the daily happening that are taking place on
+the farm that they are invested in and this comes to them inform of newsfeedsin both
+video, graphic and writting from the seedfruit team using the seedfruit forum and
+working group
+</div>
 
 </div>
+<div class="row" style="margin-top:50px ;padding-bottom:50px; background-color:blue;color:white;align-content:center">
+<div class="col-4">
+$462B<br>
+Quarterly volume traded
+    </div>
+    <div class="col-4" style="justify-content: center;align-items:center">
+    100+<br>
+Countries supported
+    </div>
+    <div class="col-4">
+    68+M<br>
+Verified users
+    </div>
+
 </div>
-<div class="row"></div>
-<div class="row"></div>
+<div class="row">
+    
+</div>
 <div class="row"></div>
 <div class="row"></div>
 </div>
